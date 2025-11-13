29b95a1095efbf89d9900fe4716b0b257a136eb6
smartpromoai <smartpromoai@gmail.com>
2025-11-13 22:15:21 +0300

    chore: use next.config.js (CJS) instead of next.config.mjs

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/aida-public/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
